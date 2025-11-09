fe432f693e72635fefae88298476bc2e4c6a9e1c
marcinamovgorda2 <marcinamovgorda2@users.noreply.github.com>
2025-11-09 12:13:56 +0000

    Add module.js, mutations-analytics.sass, pages-utils-simple.yaml, payment-example.js

---
+++ frontend/web/src/module.js
@@ -0,0 +1,5 @@
+import { ethers } from 'ethers';
+
+export default function adapter() { return <div>Component</div>; }
+
+
---
+++ frontend/web/src/payment-example.js
@@ -0,0 +1,5 @@
+import { ethers } from 'ethers';
+
+interface dist_complexProps { data: string }
+
+
